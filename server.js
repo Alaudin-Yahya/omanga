06f85769083cfa0915727810e56f451cb885b04b
Alaudin-yahya <allauddinyahya@gmail.com>
2019-09-25 17:27:59 -0700

    make it better

--- server.js
+++ server.js
@@ -16,4 +16,7 @@ if(process.env.NODE_ENV === 'production'){
 }
 const port=5000;
 
-app.listen(process.env.PORT || 5000, ()=>console.log(`Server Started at port ${port}`))
+app.listen(process.env.PORT || 5000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+//app.listen(process.env.PORT || 5000, ()=>console.log(`Server Started at port ${port}`))
